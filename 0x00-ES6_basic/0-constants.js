01bc60690a962eed5a78141dac6cc45dc63efc8d
Blessing Victor <128801000+Blessingngo@users.noreply.github.com>
2024-06-26 03:01:37 +0100

    Update 0-constants.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,9 +1,15 @@
-function taskFirst() {
-  const task = 'First task';
-  console.log(task);
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
 }
 
-function taskNext() {
-  let task = 'Next task';
-  console.log(task);
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
 }
