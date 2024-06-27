08ec1902e23b502428f56153274ddd5efd507c0d
Blessing Victor <128801000+Blessingngo@users.noreply.github.com>
2024-06-28 00:54:28 +0100

    Create 5-building.js

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,20 @@
+class Building {
+  constructor(sqft) {
+    if (this.constructor === Building) {
+      throw new Error("Building is an abstract class and cannot be instantiated directly");
+    }
+    this._sqft = sqft;
+  }
+
+  // Getter for sqft
+  get sqft() {
+    return this._sqft;
+  }
+
+  // Abstract method enforcement
+  evacuationWarningMessage() {
+    throw new Error("Class extending Building must override evacuationWarningMessage");
+  }
+}
+
+export default Building;
